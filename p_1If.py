1ac9b992793997d8b5c532ac59c3e8e7fcadb89b
BufteacEcaterina03 <70895514+BufteacEcaterina03@users.noreply.github.com>
2020-10-18 21:51:27 +0300

    Add files via upload

---
+++ p_1If.py
@@ -0,0 +1,12 @@
+e1=int(input("introduceti nr elevului :"))
+e2=int(input("introduceti nr elevului :")) 
+e3=int(input("introduceti nr elevului :"))
+p1=int(input("introduceti punctajul 1="))
+p2=int(input("introduceti punctajul2="))
+p3=int(input("introduceti punctajul 3="))
+if (p1>p2) and (p1>p3) :
+    print (e1)
+if  (p2>p1) and (p2>p3):
+    print (e2)
+if (p3>p1) and (p3>p2):
+    print (e3)  
